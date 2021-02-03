de0cb85a2d1b5fd8c6185e24619f713c2c3cdbcd
Dieptran <dieptt.hust@gmail.com>
2021-02-04 00:23:44 +0700

    update 2

---
+++ LearningJavaSwing/src/diep/JButtonDemo/SwingControlDemo.java
@@ -0,0 +1,26 @@
+package diep.JButtonDemo;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SwingControlDemo {
+	
+	private JFrame mainFrame;
+	private JLabel headerLabel;
+	private JLabel statusLabel;
+	private JPanel controlPanel;
+	
+	public SwingControlDemo() {
+		prepareGUI();
+	}
+	
+	public static void main(String[] args) {
+		SwingControlDemo swingControlDemo = new SwingControlDemo();
+		swingControlDemo.showButtonDemo();
+	}
+	
+	private void prepareGUI() {
+		
+	}
+}
