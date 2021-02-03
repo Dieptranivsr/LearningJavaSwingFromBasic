6e950793a39ff0b6b73f6c6639c5a1fb8dccf7f5
Dieptran <dieptt.hust@gmail.com>
2021-02-03 22:42:00 +0700

    update 1

--- LearningJavaSwing/src/JLabelDemo/SwingControlDemo.java
+++ LearningJavaSwing/src/JLabelDemo/SwingControlDemo.java
@@ -9,7 +9,7 @@ public class SwingControlDemo {
 	private JFrame mainFrame;
 	private JLabel headerLabel;
 	private JLabel statusLabel;
-	private JLabel controlPanel;
+	private JPanel controlPanel;
 	
 	public SwingControlDemo() {
 		prepareGUI();
@@ -23,7 +23,40 @@ public class SwingControlDemo {
 	
 	private void prepareGUI() {
 		mainFrame = new JFrame("Vi du mo dau Java Swing");
+		//mainFrame.setMaximumSize(null);
 		mainFrame.setSize(400, 400);
+		mainFrame.setLayout(new GridLayout(3, 1));
+		mainFrame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent windowEvent) {
+				System.exit(0);
+			}
+		});
+		headerLabel = new JLabel("", JLabel.CENTER);
+		statusLabel = new JLabel("", JLabel.CENTER);
+		
+		statusLabel.setSize(350, 100);
+		
+		controlPanel = new JPanel();
+		controlPanel.setLayout(new FlowLayout());
+		
+		mainFrame.add(headerLabel);
+		mainFrame.add(controlPanel);
+		mainFrame.add(statusLabel);
+		mainFrame.setVisible(true);
 	}
-
+	
+	private void showLabelDemo() {
+		headerLabel.setText("Control in action: JLabel");
+		
+		JLabel label = new JLabel("", JLabel.CENTER);
+		
+		label.setText("Welcome to Java Swing instruction lesson.");
+		label.setOpaque(true);
+		label.setBackground(Color.GRAY);
+		label.setForeground(Color.WHITE);
+		
+		controlPanel.add(label);
+		
+		mainFrame.setVisible(true);
+	}
 }
